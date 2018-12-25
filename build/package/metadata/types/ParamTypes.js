b5140e29a0fd977c0fba100554d1657fec9646e5
Chris Sim <monkeymon@gmail.com>
2018-12-25 17:06:23 +0800

    stable for use

---
+++ build/package/metadata/types/ParamTypes.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * Controller action's parameter type.
+ */
+var ParamTypes = /** @class */ (function () {
+    function ParamTypes() {
+    }
+    ParamTypes.CUSTOM = "custom";
+    ParamTypes.CONNECTED_SOCKET = "connected_socket";
+    ParamTypes.SOCKET_BODY = "socket_body";
+    ParamTypes.SOCKET_QUERY_PARAM = "socket_query_param";
+    ParamTypes.SOCKET_IO = "socket_io";
+    ParamTypes.SOCKET_ID = "socket_id";
+    ParamTypes.SOCKET_REQUEST = "socket_request";
+    ParamTypes.SOCKET_ROOMS = "socket_rooms";
+    return ParamTypes;
+}());
+exports.ParamTypes = ParamTypes;
+
+//# sourceMappingURL=ParamTypes.js.map
